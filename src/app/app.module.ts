58cea32c6395ccdfc891385fa4c830af225f81f7
Luan Chuong Nguyen <lcnguyen@codixfr.private>
2020-01-10 15:25:21 +0700

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { ModelModule } from "./model/model.module"; 
+import { CoreModule } from "./core/core.module"; 
+import { TableComponent } from "./core/table.component"; 
+import { FormComponent } from "./core/form.component"; 
+import { MessageModule } from "./messages/message.module"; 
+import { MessageComponent } from "./messages/message.component"
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, ModelModule, CoreModule, MessageModule],  
+  bootstrap: [TableComponent, FormComponent, MessageComponent]
 })
 export class AppModule { }
