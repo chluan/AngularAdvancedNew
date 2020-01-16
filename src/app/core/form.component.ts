e8f3f63b7a9c80d62edf1311213899f24e829ec4
Luan Chuong Nguyen <lcnguyen@codixfr.private>
2020-01-16 17:47:57 +0700

    fifthcommit

--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -1,9 +1,10 @@
-import { Component,Inject } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Product } from "../model/product.model";
 import { Model } from "../model/repository.model";
 import { MODES, SharedState, SHARED_STATE } from "./sharedState.model";
 import { Observable } from "rxjs";
+import { filter, map, distinctUntilChanged } from "rxjs/operators";
 
 @Component({
     selector: "paForm",
@@ -14,27 +15,32 @@ export class FormComponent {
 
     product: Product = new Product();
 
-    constructor(private model: Model, 
-        @Inject(SHARED_STATE) private stateEvents: Observable<SharedState>) { 
-            stateEvents.subscribe((update) => {
-                this.product = new Product();
-                if(update.id != undefined){
-                    Object.assign(this.product, this.model.getProduct(update.id));
-                }
-                this.editing = update.mode == MODES.EDIT;
-        })
+    constructor(private model: Model,
+        @Inject(SHARED_STATE) private stateEvents: Observable<SharedState>) {
+        stateEvents
+            //.pipe(map(state => new SharedState(state.mode, state.id == 5 ? state.id = 1 : state.id)))
+            .pipe(map(state => state.mode == MODES.EDIT ? state.id : -1))
+            .pipe(distinctUntilChanged())
+            .pipe(filter(id => id != 3))
+            .subscribe((id) => { 
+                this.editing = id != -1; 
+                this.product = new Product(); 
+                if (id != -1) { 
+                    Object.assign(this.product, this.model.getProduct(id)) 
+                } 
+            });
     }
 
     // get editing(): boolean {
     //     return this.state.mode == MODES.EDIT;
     // }
 
-    editing : boolean = false;
+    editing: boolean = false;
 
     submitForm(form: NgForm) {
         if (form.valid) {
-            this.model.saveProduct(this.product); 
-            this.product = new Product(); 
+            this.model.saveProduct(this.product);
+            this.product = new Product();
             form.reset();
         }
     }
