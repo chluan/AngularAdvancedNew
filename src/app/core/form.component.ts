6eb7ed64a2dd9e2b3a4361e3ae8c05bda2fc61d5
Luan Chuong Nguyen <lcnguyen@codixfr.private>
2020-01-17 16:50:54 +0700

    sixth

--- src/app/core/form.component.ts
+++ src/app/core/form.component.ts
@@ -4,7 +4,7 @@ import { Product } from "../model/product.model";
 import { Model } from "../model/repository.model";
 import { MODES, SharedState, SHARED_STATE } from "./sharedState.model";
 import { Observable } from "rxjs";
-import { filter, map, distinctUntilChanged } from "rxjs/operators";
+import { filter, map, distinctUntilChanged, first } from "rxjs/operators";
 
 @Component({
     selector: "paForm",
@@ -18,16 +18,24 @@ export class FormComponent {
     constructor(private model: Model,
         @Inject(SHARED_STATE) private stateEvents: Observable<SharedState>) {
         stateEvents
-            //.pipe(map(state => new SharedState(state.mode, state.id == 5 ? state.id = 1 : state.id)))
-            .pipe(map(state => state.mode == MODES.EDIT ? state.id : -1))
-            .pipe(distinctUntilChanged())
-            .pipe(filter(id => id != 3))
-            .subscribe((id) => { 
-                this.editing = id != -1; 
-                this.product = new Product(); 
-                if (id != -1) { 
-                    Object.assign(this.product, this.model.getProduct(id)) 
-                } 
+            // .pipe(map(state => state.mode == MODES.EDIT ? state.id : -1))
+            // .pipe(distinctUntilChanged())
+            // .pipe(filter(id => id != 3))
+            // .subscribe((id) => { 
+            //     this.editing = id != -1; 
+            //     this.product = new Product(); 
+            //     if (id != -1) { 
+            //         Object.assign(this.product, this.model.getProduct(id)) 
+            //     } 
+            // });
+            .pipe(distinctUntilChanged((firstState, secondState) => 
+                firstState.mode == secondState.mode && firstState.id == secondState.id))
+            .subscribe(update => {
+                this.product = new Product();
+                if (update.id != undefined) {
+                    Object.assign(this.product, this.model.getProduct(update.id))
+                }
+                this.editing = update.mode == MODES.EDIT;
             });
     }
 
